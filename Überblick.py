f0647be06c320eb008f9899d85cf2a9e909567e3
Martin Writz <martin.writz@ktn.gv.at>
2025-10-20 07:59:16 +0200

    version 2025

--- Überblick.py
+++ Überblick.py
@@ -19,7 +19,7 @@ st.set_page_config(page_title="Dashboard der Landesstelle für Statistik", layou
 ## CUSTOM CSS
 st.markdown(get_custom_css(), unsafe_allow_html=True)
 
-insert_styling(250, 191, 116, 1, 255, 255, 255, 1)
+insert_styling(226, 226, 226, 1, 255, 255, 255, 1)
 
 with st.sidebar:
     cover_img = ImageOps.expand(Image.open("img/cover_hb.PNG"), border=4, fill='white')
@@ -27,7 +27,7 @@ with st.sidebar:
     st.image(cover_img)
                
     st.markdown("""
-    <a href="https://www.ktn.gv.at/DE/repos/files/ktn.gv.at/Abteilungen/Abt1/Dateien/PDF/Statistik/Publikationen%5fStat/Statistisches%5fHandbuch/2024%5f12%5f19%5fHandbuch%5fv2%5fverlinkt%5fcomp.pdf?exp=1584214&fps=6bd0ae5d72df8a699ee88fe30fff59bc9ec9d39e" target="_blank">
+    <a href="https://www.ktn.gv.at/DE/repos/files/ktn.gv.at/Abteilungen/Abt1/Dateien/PDF/Statistik/Publikationen%5fStat/Statistisches%5fHandbuch/Handbuch%5fonline%5fcomp%5fv2.pdf" target="_blank">
         <button style="padding:10px 20px; background-color:#4CAF50; color:white; border:none; border-radius:5px; cursor:pointer;">
             Download
         </button>
@@ -40,9 +40,9 @@ with st.sidebar:
 
     with st.expander("Info"):
         st.write('''
-            Dashboard der Landesstelle für Statistik, Amt der Kärntner Landesregierung, aktualisiert am 07.07.2025, erstellt von Martin Writz, BSc.
+            Dashboard der Landesstelle für Statistik, Amt der Kärntner Landesregierung, aktualisiert am 20.10.2025, erstellt von Martin Writz, BSc.
             
-            please report bugs to martin.writz@ktn.gv.at or abt1.statistik@ktn.gv.at, feel free to contribute or commit a pull request directly
+            please report bugs to martin.writz@ktn.gv.at or statistik@ktn.gv.at, feel free to contribute or commit a pull request directly
         ''')
 
 st.markdown(f"""<h1>Dashboard der Landesstelle für Statistik</h1>""", unsafe_allow_html=True)
